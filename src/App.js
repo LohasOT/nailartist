8f4a5653a824f53b3cdceb2d20cdeeedb5cd8d97
LohasOT <oliverhoang93@gmail.com>
2022-03-15 01:23:32 -0700

    update links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Appbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Nails from "./components/Nails/Nails"
+import Top from "./components/Top/Top";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Appbar></Appbar>
+      <Footer></Footer>
+    </>
   );
 }
 
