c8e1cb5266f19e54f3e590539cb4d6762100ce80
LohasOT <oliverhoang93@gmail.com>
2022-03-20 00:56:44 -0700

    setting layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Appbar from './components/Navbar/Appbar'
-import Nails from './components/Nails/Nails'
+import Appbar from './components/Navbar/Appbar';
+import Nails from './components/Nails/Nails';
+import Main from './components/Main/Main';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <>
     <Appbar></Appbar>
+    <Main></Main>
     <Nails></Nails>
     </>
   );
---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+function Main() {
+  return (
+    <>
+      <h1>Welcome</h1>
+    </>
+  );
+}
+
+export default Main
