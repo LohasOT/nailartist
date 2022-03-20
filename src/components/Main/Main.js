b7447a40a6059a899a399e1bd42c37e836721892
LohasOT <oliverhoang93@gmail.com>
2022-03-20 16:51:32 -0700

    centering

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
+import './Main.css'
 
 function Main() {
   return (
     <>
       <br></br><br></br><br></br>
-      <h2>Nail Collection</h2>
-      <h2>By Andy Huynh</h2>
+      <h2 className='title'>Nail Collection</h2>
+      <h2 className='title'>By Andy Huynh</h2>
+      <br></br><br></br>
     </>
   );
 }
